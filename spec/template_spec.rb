15ee70e83b5bf52c9ab0948f974bce10d2b4d123
Franklin Huichi <carloshuichi@gmail.com>
2022-02-22 18:33:57 -0500

    Adding test to template_spec.rb We added some test to template_spec.rb and we used the rubycritic and we have a good point. Also we execute sandi meter and we have a good point.
    Co-authored-by: DiegoSteve <diegosteve123321@gmail.com>
    Co-authored-by: HeikelV <heikelvillar@gmail.com>

--- spec/template_spec.rb
+++ spec/template_spec.rb
@@ -2,16 +2,32 @@
 
 require 'rspec'
 require_relative '../app/template'
+require_relative '../app/cell'
 
 describe Template do
-  it 'check grid size' do
-    template = Template.new(3, 8)
-    template.generate
-    expect(template.one_line_grid.size).to eq 24
+
+  describe 'one_line_grid' do
+    
+    it 'check grid size' do
+      template = Template.new(3, 8)
+      template.generate
+      expect(template.one_line_grid.size).to eq 24
+    end
+
   end
 
-  it 'check grid size of column' do
-    template = Template.new(3, 8)
-    expect(template.generate[0].size).to eq 8
+  describe 'generate' do
+    
+    it 'check grid size of column' do
+      template = Template.new(3, 8)
+      expect(template.generate[0].size).to eq 8
+    end
+
+    it 'check grid size of rows' do
+      template = Template.new(3, 8)
+      expect(template.generate.size).to eq 3
+    end
+    
   end
+
 end
